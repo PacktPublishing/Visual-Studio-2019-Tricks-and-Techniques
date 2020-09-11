7be13d90cbbfb9916789b04f0720bc89ac506a45
Paul Schroeder <paul@msctek.com>
2020-09-10 21:01:34 -0500

    CH9 - initial commit

---
+++ CH9 - Create Your Own Templates/Start/CitySelector.WPF/Service/ICitySelectorService.cs
@@ -0,0 +1,16 @@
+﻿using CitySelector.WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CitySelector.WPF.Service
+{
+    public interface ICitySelectorService
+    {
+        Task<IList<City>> GetCities(Guid stateId);
+
+        Task<IList<Country>> GetCountries();
+
+        Task<IList<State>> GetStates(Guid countryId);
+    }
+}
---
+++ CH9 - Create Your Own Templates/Start/CitySelector.WPF/View/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CitySelector.WPF.Data;
+using CitySelector.WPF.Service;
+using CitySelector.WPF.ViewModel;
+using Microsoft.Extensions.Logging;
+using System.Windows;
+
+namespace CitySelector.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(ILogger<MainWindowViewModel> logger, ICitySelectorService citySelectorService)
+        {
+            MainWindowViewModel vm = new MainWindowViewModel(logger, citySelectorService);
+            this.DataContext = vm;
+
+            InitializeComponent();
+        }
+    }
+}
