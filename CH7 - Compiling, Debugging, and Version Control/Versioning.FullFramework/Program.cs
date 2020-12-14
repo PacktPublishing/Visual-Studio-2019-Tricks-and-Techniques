b54601614ed10c701476762ff7a244aa1ef2b12e
Paul Schroeder <paul@msctek.com>
2020-12-14 02:23:21 -0600

    CH7 - Adding Versioning.FullFramework project for reader's reference.

---
+++ CH7 - Compiling, Debugging, and Version Control/Versioning.FullFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var assyVersion = Assembly.GetEntryAssembly()
+  .GetName().Version;
+            var fvi = FileVersionInfo.GetVersionInfo(Assembly
+              .GetExecutingAssembly().Location);
+            string fileVersion = fvi.FileVersion;
+            Console.WriteLine($"Assembly Version: {assyVersion}");
+            Console.WriteLine($"File Version: {fileVersion}");
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
