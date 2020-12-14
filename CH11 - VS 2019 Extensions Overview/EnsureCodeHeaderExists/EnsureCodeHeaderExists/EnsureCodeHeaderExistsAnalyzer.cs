4a7a7abf209700aff4adebf7a340bf5ff3e84bcd
User <User@WINDEV2011EVAL>
2020-12-13 20:44:37 -0800

    Added Roslyn Header Search Rule.

---
+++ CH11 - VS 2019 Extensions Overview/EnsureCodeHeaderExists/EnsureCodeHeaderExists/EnsureCodeHeaderExistsAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace EnsureCodeHeaderExists
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class EnsureCodeHeaderExistsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "EnsureCodeHeaderExists";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxTreeAction(AnalyzeComment);
+        }
+
+        private void AnalyzeComment(SyntaxTreeAnalysisContext context)
+        {
+            SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+            var fileName = Path.GetFileName(context.Tree.FilePath);
+            var text = context.Tree.ToString();
+
+            if (!text.StartsWith("//\r\n// Copyright (c) Visual Studio 2019 Tips and Tricks. All rights reserved.\r\n// Licensed under the Creative Commons licence. See LICENSE file in the project root for full license information.\r\n//\r\n\r\n"))
+            {
+                var diagnostic = Diagnostic.Create(Rule, Location.Create(context.Tree, new TextSpan(0, 0)), fileName);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
