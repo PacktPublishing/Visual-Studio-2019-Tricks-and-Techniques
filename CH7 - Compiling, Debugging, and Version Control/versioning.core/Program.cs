16a4a9362d136ba8ad040eb8bcc4a23ca14595d0
Paul Schroeder <paul@msctek.com>
2020-12-14 00:59:34 -0600

    CH7 - Added Console.ReadKey() to pause CH7 before closing output window. Added a .sln file for VS for Win.

--- CH7 - Compiling, Debugging, and Version Control/versioning.core/Program.cs
+++ CH7 - Compiling, Debugging, and Version Control/versioning.core/Program.cs
@@ -4,15 +4,18 @@ using System.Reflection;
 
 namespace versioning.core
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var assyVersion = Assembly.GetEntryAssembly().GetName().Version;
             var fvi = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
             string fileVersion = fvi.FileVersion;
             Console.WriteLine($"Assembly Version: {assyVersion}");
             Console.WriteLine($"File Version: {fileVersion}");
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey(); // Pause so the console output can be read.
         }
     }
 }
